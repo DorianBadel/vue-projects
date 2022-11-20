ec507b52bf106175e630fc8b8fe6b36180c2d54a
Dorian Badel <DorianBadel@protonmail.com>
2022-11-20 16:53:38 +0100

    Project setup

---
+++ todo-list-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo"/>
+      <h1>Pinia todo</h1>
+    </header>
+
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
